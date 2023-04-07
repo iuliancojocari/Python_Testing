5049fdaede1a84a1383658c8fd1e48a2fffb40f0
Iulian COJOCARI <iuliancojocari42@gmail.com>
2023-04-07 23:32:06 +0200

    use available points bug fixed

---
+++ tests/unit_tests/test_use_available_points_only.py
@@ -0,0 +1,49 @@
+def test_use_available_points(client):
+    club = {
+        "name":"Simply Lift",
+        "email":"john@simplylift.co",
+        "points":"13"
+    }
+
+    competition = {
+            "name": "Spring Festival",
+            "date": "2020-03-27 10:00:00",
+            "numberOfPlaces": "25"
+        }
+    
+    data = {
+        "places" : 8,
+        "club" : club["name"],
+        "competition": competition["name"]
+    }
+
+    response = client.post('/purchasePlaces', data=data)
+
+    message = response.data.decode()
+    assert response.status_code == 200
+    assert "Great-booking complete!" in message
+
+def test_use_more_points_than_available(client):
+    club = {
+        "name":"Simply Lift",
+        "email":"john@simplylift.co",
+        "points":"13"
+    }
+
+    competition = {
+            "name": "Spring Festival",
+            "date": "2020-03-27 10:00:00",
+            "numberOfPlaces": "25"
+        }
+    
+    data = {
+        "places" : 14,
+        "club" : club["name"],
+        "competition": competition["name"]
+    }
+
+    response = client.post('/purchasePlaces', data=data)
+
+    message = response.data.decode()
+    assert response.status_code == 400
+    assert "Insuficient points !" in message
